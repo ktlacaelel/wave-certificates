44d8b76bd59c0789e5c5666033ae791a39cbdcf9
ktlacaelel <kazu@paz.local>
2009-10-02 23:42:40 +0900

    added ca certificate generator to test shell script

--- test.sh
+++ test.sh
@@ -1,20 +1,27 @@
 
+variable=$1
 
 # STEP ONE: create certificate
 # ============================
 
-. build_certificates.sh test
+. build_certificates.sh $variable
 
 
 # STEP TWO: dump certificate
 # ==========================
 
-. dump_certificate.sh test
+. dump_certificate.sh $variable
 
 
 # STEP THREE: check certificate
 # =============================
 
-. check_certificate.sh test
+. check_certificate.sh $variable
+
+
+# STEP FOUR: generate a ca issued certificate
+# ============================================
+
+. get_ca_issued_certificate.sh $variable
 
 
